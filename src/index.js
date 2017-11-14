ec5245baee61375f9651fdb86cef862f3dfef05d
SergioBurlaka <burlaka01@gmail.com>
2017-11-14 23:52:15 +0200

    routing back to react-router v3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// // import registerServiceWorker from './registerServiceWorker';
+//
+//
+// import {composeWithDevTools} from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+//
+//
+// import  {Provider} from 'react-redux';
+// import  {createStore, applyMiddleware} from 'redux';
+//
+//
+// import reducer from './reducers/index';
+// // import { Router, Route, browserHistory, Switch  } from 'react-router';
+// // import { Router, Route, Switch  } from 'react-router';
+// import {   Switch  } from 'react-router';
+// import { syncHistoryWithStore,ConnectedRouter } from 'react-router-redux';
+//
+// import UsersTable from './containers/UsersTable';
+// import AddUser from './containers/AddUser';
+// import CurrencyCalculator from './containers/CurrencyCalculator';
+//
+// import axios from 'axios';
+// import { BrowserRouter } from 'react-router-dom';
+// // import createHistory from 'history/createBrowserHistory'
+//
+// import { Router, Route, browserHistory } from 'react-router'
+//
+//
+//
+//
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+// import 'react-select/dist/react-select.css';
+//
+// const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+// const history = syncHistoryWithStore(browserHistory,store);
+// // const history = createHistory(store);
+//
+//
+//
+// axios.get(`https://jsonplaceholder.typicode.com/users`)
+//     .then(res => {
+//         store.dispatch((dispatch) =>{
+//             dispatch({type:'ADD_USERS', payload: res.data});
+//             dispatch({type:'CALCULATE_PAGINATION', payload: res.data});
+//         })
+//     });
+//
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -13,28 +65,21 @@ import  {createStore, applyMiddleware} from 'redux';
 
 
 import reducer from './reducers/index';
-// import { Router, Route, browserHistory, Switch  } from 'react-router';
-// import { Router, Route, Switch  } from 'react-router';
-import {   Switch  } from 'react-router';
+import { Router, Route, browserHistory  } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { ConnectedRouter } from 'react-router-redux';
 
 import UsersTable from './containers/UsersTable';
 import AddUser from './containers/AddUser';
 import CurrencyCalculator from './containers/CurrencyCalculator';
 
 import axios from 'axios';
-import { Route, BrowserRouter } from 'react-router-dom';
-
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'react-select/dist/react-select.css';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-// const history = syncHistoryWithStore(store);
-
-
+const history = syncHistoryWithStore(browserHistory, store);
 
 axios.get(`https://jsonplaceholder.typicode.com/users`)
     .then(res => {
@@ -47,16 +92,29 @@ axios.get(`https://jsonplaceholder.typicode.com/users`)
 
 
 ReactDOM.render(
-    <BrowserRouter>
-         <Provider store={store} >
-             <Switch>
-                 <Route  path="/" exact component={UsersTable}/>
-                 <Route  path="/addUser"  component={AddUser}/>
-                 <Route  path="/currencyCalculator"  component={CurrencyCalculator}/>
-             </Switch>
-        </Provider>
-    </BrowserRouter>
+        //  <Provider store={store} >
+        //      <ConnectedRouter history={history}>
+        //         <div>
+        //             <Route  path="/" exact component={UsersTable}/>
+        //             <Route  path="/addUser"  component={AddUser}/>
+        //             <Route  path="/currencyCalculator"  component={CurrencyCalculator}/>
+        //         </div>
+        //      </ConnectedRouter>
+        // </Provider>
+
+    <Provider store={store}>
+        <Router history={history}>
+            <Route  path="/"  component={UsersTable}/>
+            <Route  path="/addUser"  component={AddUser}/>
+            <Route  path="/currencyCalculator"  component={CurrencyCalculator}/>
+        </Router>
+    </Provider>
     , document.getElementById('root'));
+
+// <Switch> </Switch>
+
+// <BrowserRouter></BrowserRouter>
+
 // registerServiceWorker();
 
 // <Provider store={store} >
