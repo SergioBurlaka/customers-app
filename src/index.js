20eb352324857ed78b01f19af2cc8b4c72a71677
SergioBurlaka <burlaka01@gmail.com>
2017-11-14 11:49:14 +0200

    8 commit update routing using react-router 4

--- src/index.js
+++ src/index.js
@@ -13,21 +13,25 @@ import  {createStore, applyMiddleware} from 'redux';
 
 
 import reducer from './reducers/index';
-import { Router, Route, browserHistory  } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+// import { Router, Route, browserHistory, Switch  } from 'react-router';
+// import { Router, Route, Switch  } from 'react-router';
+import {  Route, Switch  } from 'react-router';
+// import { syncHistoryWithStore } from 'react-router-redux';
 
 import UsersTable from './containers/UsersTable';
 import AddUser from './containers/AddUser';
 import CurrencyCalculator from './containers/CurrencyCalculator';
 
 import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'react-select/dist/react-select.css';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-// const history = syncHistoryWithStore(browserHistory, store);
+// const history = syncHistoryWithStore( store);
 
 
 
@@ -42,19 +46,29 @@ axios.get(`https://jsonplaceholder.typicode.com/users`)
 
 
 ReactDOM.render(
-        <Provider store={store} >
-            <div>
-                <UsersTable/>
-                <AddUser/>
-                <CurrencyCalculator/>
-            </div>
+    <BrowserRouter>
+         <Provider store={store} >
+             <Switch>
+                 <Route  path="/" exact component={UsersTable}/>
+                 <Route  path="/addUser" exact component={AddUser}/>
+                 <Route  path="/currencyCalculator" exact component={CurrencyCalculator}/>
+             </Switch>
         </Provider>
-    // <Provider store={store}>
-    //     <Router history={history}>
-    //         <Route path="/" component={UsersTable}/>
-    //         <Route path="/addUser" component={AddUser}/>
-    //         <Route path="/currencyCalculator" component={CurrencyCalculator}/>
-    //     </Router>
-    // </Provider>
+    </BrowserRouter>
     , document.getElementById('root'));
 // registerServiceWorker();
+
+// <Provider store={store} >
+//     <div>
+//         <UsersTable/>
+//         <AddUser/>
+//         <CurrencyCalculator/>
+//     </div>
+// </Provider>
+// <Provider store={store}>
+//     <Router history={history}>
+//         <Route path="/" component={UsersTable}/>
+//         <Route path="/addUser" component={AddUser}/>
+//         <Route path="/currencyCalculator" component={CurrencyCalculator}/>
+//     </Router>
+// </Provider>
